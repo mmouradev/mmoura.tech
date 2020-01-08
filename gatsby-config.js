5feb03aac3c3b86632bb5563437e4c9c90c82094
Matheus Moura <matheus@setconsultoria.com.br>
2020-01-08 20:05:24 -0300

    changing icon and gatsby configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,4 @@
 module.exports = {
-  siteMetadata: {
-    title: `Matheus Moura`,
-    position: `Front-end Developer`,
-    description: `Um pouco mais do meu mundo como desenvolvedor front-end e outras coisas mais.`,
-    author: `@mmouradev`,
-    siteUrl: 'https://mmoura.tech',
-  },
   plugins: [
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-netlify-cms`,
@@ -63,14 +56,18 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2195cf`,
+        theme_color: `#2195cf`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
+  siteMetadata: {
+    title: `Matheus Moura`,
+    position: `Front-end Developer`,
+    description: `Um pouco mais do meu mundo como desenvolvedor front-end e outras coisas mais.`,
+    author: `@mmouradev`,
+    siteUrl: 'https://mmoura.tech',
+  },
 };
